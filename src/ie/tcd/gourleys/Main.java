8e4b947805d566905510b7c20de160aa4b182b3e
Scarlett Gourley <gourleys@tcd.ie>
2015-06-05 18:13:50 +0100

    Implemented added changes thus far except for obstacle layer in MapMaker

--- src/ie/tcd/gourleys/Main.java
+++ src/ie/tcd/gourleys/Main.java
@@ -1,5 +1,8 @@
 package ie.tcd.gourleys;
 
+import java.io.FileNotFoundException;
+
+import ie.tcd.gourleys.entities.Playable;
 import ie.tcd.gourleys.maps.MapMaker;
 import processing.core.PApplet;
 import processing.core.PImage;
@@ -7,27 +10,44 @@ import processing.core.PImage;
 @SuppressWarnings("serial")
 public class Main extends PApplet {
 	
-	public static final int CANVAS_WIDTH = 1000;
-	public static final int CANVAS_HEIGHT = 750;
-	PImage texture;
-	MapMaker grass;
-	int[][] indexes = {{0, 1, 2, 2},
-						{3, 4, 5, 4},
-						{6, 7, 8, 6}};
+	public static final int CANVAS_WIDTH = 300;
+	public static final int CANVAS_HEIGHT = 300;
+	public static final int TILE_ROW = 20;
+	public static final int TILE_COL = 20;
+	MapMaker map;
+	Playable player;
+	int[][] indexes = 	{{3, 3, 2, 3, 3},
+						{0, 1, 0, 0, 3},
+						{6, 6, 7, 1, 2},
+						{1, 0, 8, 6, 7},
+						{3, 3, 3, 2, 11}};
 
 	
 	public void setup() {
-		texture = loadImage("img/grass_texture.png");
-		grass = new MapMaker(this, CANVAS_WIDTH, CANVAS_HEIGHT, 10, 10);
-		grass.slice(texture, 6, 6);
-//		grass.setTiles(indexes);
-		grass.randomTiles();
+		map = new MapMaker(this, CANVAS_WIDTH, CANVAS_HEIGHT, TILE_ROW, TILE_COL);
+		PImage texture = loadImage("img/pkmn/base_grass.png");
+		map.slice(texture, 2, 3);
+		map.randomBase();
+		
+		PImage[] character = {loadImage("img/nekid/nekid_down.png"), loadImage("img/nekid/nekid_up.png"),
+							loadImage("img/nekid/nekid_right.png"), loadImage("img/nekid/nekid_left.png")};
+		player = new Playable(this, character, CANVAS_WIDTH/2, CANVAS_HEIGHT/2);
+//		player.resize(CANVAS_WIDTH, CANVAS_HEIGHT, TILE_ROW, TILE_COL);
 		
 		size(CANVAS_WIDTH, CANVAS_HEIGHT);
 	}
 	
 	public void draw() {
-		grass.draw();
+		map.draw();
+		player.draw();
+	}
+	
+	public void keyPressed() {
+		player.keyPressed();
+	}
+	
+	public void keyReleased() {
+		player.keyReleased();
 	}
 
 }
